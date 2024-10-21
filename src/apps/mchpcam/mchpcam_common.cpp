a7189b6618828b5341b62288568d968ed8b06665
Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>
2024-10-21 14:19:03 +0530

    apps: mchpcam: Fix indentation
    Fix the tab spaces in init function
    
    Signed-off-by: Balamanikandan Gunasundar <balamanikandan.gunasundar@microchip.com>

--- src/apps/mchpcam/mchpcam_common.cpp
+++ src/apps/mchpcam/mchpcam_common.cpp
@@ -38,24 +38,24 @@ int MchpCamCommon::init(const std::string &cameraId)
 	cameraManager_->start();
 
 	if (cameraId.empty()) {
-	auto cameras = cameraManager_->cameras();
-	if (cameras.empty()) {
-	    std::cerr << "No cameras available" << std::endl;
-	    return -1;
-	}
-	camera_ = cameraManager_->get(cameras[0]->id());
+		auto cameras = cameraManager_->cameras();
+		if (cameras.empty()) {
+			std::cerr << "No cameras available" << std::endl;
+			return -1;
+		}
+		camera_ = cameraManager_->get(cameras[0]->id());
 	} else {
-	camera_ = cameraManager_->get(cameraId);
+		camera_ = cameraManager_->get(cameraId);
 	}
 
 	if (!camera_) {
-	std::cerr << "Failed to find camera" << std::endl;
-	return -1;
+		std::cerr << "Failed to find camera" << std::endl;
+		return -1;
 	}
 
 	if (camera_->acquire() < 0) {
-	std::cerr << "Failed to acquire camera" << std::endl;
-	return -1;
+		std::cerr << "Failed to acquire camera" << std::endl;
+		return -1;
 	}
 
 	config_ = camera_->generateConfiguration({ StreamRole::Viewfinder });
@@ -66,32 +66,34 @@ int MchpCamCommon::init(const std::string &cameraId)
 
 	CameraConfiguration::Status status = config_->validate();
 	if (status == CameraConfiguration::Invalid) {
-	std::cerr << "Invalid camera configuration" << std::endl;
-	return -1;
+		std::cerr << "Invalid camera configuration" << std::endl;
+		return -1;
 	}
+
 	if (status == CameraConfiguration::Adjusted) {
-	std::cout << "Camera configuration adjusted" << std::endl;
-	width_ = config_->at(0).size.width;
-	height_ = config_->at(0).size.height;
+		std::cout << "Camera configuration adjusted" << std::endl;
+		width_ = config_->at(0).size.width;
+		height_ = config_->at(0).size.height;
 	}
 
 	if (camera_->configure(config_.get()) < 0) {
-	std::cerr << "Failed to configure camera" << std::endl;
-	return -1;
+		std::cerr << "Failed to configure camera" << std::endl;
+		return -1;
 	}
 
 	std::cout << "Actual configured resolution: " << width_ << "x" << height_ << std::endl;
 
 	allocator_ = std::make_unique<FrameBufferAllocator>(camera_);
 	for (StreamConfiguration &cfg : *config_) {
-	if (allocator_->allocate(cfg.stream()) < 0) {
-	    std::cerr << "Failed to allocate buffers" << std::endl;
-	    return -1;
-	}
+		if (allocator_->allocate(cfg.stream()) < 0) {
+			std::cerr << "Failed to allocate buffers" << std::endl;
+			return -1;
+		}
 	}
 
 	stream_ = config_->at(0).stream();
 	const std::vector<std::unique_ptr<FrameBuffer>> &buffers = allocator_->buffers(stream_);
+
 	for (unsigned int i = 0; i < buffers.size(); ++i) {
 		std::unique_ptr<Request> request = camera_->createRequest();
 		if (!request) {
@@ -127,6 +129,7 @@ void MchpCamCommon::stop()
 		allocator_->free(stream_);
 		camera_->release();
 	}
+
 	camera_.reset();
 	cameraManager_->stop();
 }
@@ -134,17 +137,17 @@ void MchpCamCommon::stop()
 void MchpCamCommon::setResolution(unsigned int width, unsigned int height)
 {
 	if ((width == 640 && height == 480) ||
-	(width == 1640 && height == 1232) ||
-	(width == 1920 && height == 1080) ||
-	(width == 3264 && height == 2464)) {
-	width_ = width;
-	height_ = height;
-	std::cout << "Setting resolution to: " << width_ << "x" << height_ << std::endl;
+	    (width == 1640 && height == 1232) ||
+	    (width == 1920 && height == 1080) ||
+	    (width == 3264 && height == 2464)) {
+		width_ = width;
+		height_ = height;
+		std::cout << "Setting resolution to: " << width_ << "x" << height_ << std::endl;
 	} else {
-	std::cerr << "Unsupported resolution: " << width << "x" << height
-		  << ". Falling back to default (640x480)." << std::endl;
-	width_ = 640;
-	height_ = 480;
+		std::cerr << "Unsupported resolution: " << width << "x" << height
+			  << ". Falling back to default (640x480)." << std::endl;
+		width_ = 640;
+		height_ = 480;
 	}
 }
 
